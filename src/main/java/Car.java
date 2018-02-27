a67ed99763ca4cfce49f7c6357450f600f28ad12
Ivan Atanasov <ivata89@gmail.com>
2018-02-27 22:13:48 +0000

    getting there

---
+++ src/main/java/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+
+    private final String make;
+    private final String model;
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,45 @@
-import static spark.Spark.*;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+import java.io.File;
+import java.net.URL;
+
+import static graphql.schema.idl.TypeRuntimeWiring.newTypeWiring;
+import static spark.Spark.post;
 
 public class HelloWorld {
+
+
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+
+
+        post("/graphql", (request, response) -> {
+            response.type("application/json");
+
+            SchemaParser schemaParser = new SchemaParser();
+            SchemaGenerator schemaGenerator = new SchemaGenerator();
+
+            File schemaFile = getSchemaFile();
+
+            TypeDefinitionRegistry typeRegistry = schemaParser.parse(schemaFile);
+            RuntimeWiring wiring = buildRuntimeWiring();
+            GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeRegistry, wiring);
+            return "hello";
+
+        });
+
+    }
+
+
+    private static RuntimeWiring buildRuntimeWiring() {
+        return RuntimeWiring.newRuntimeWiring().type(newTypeWiring("Car")).build();
+    }
+
+    private static File getSchemaFile() throws Exception {
+        URL dir_url = ClassLoader.getSystemResource("car.graphqls");
+        return new File(dir_url.toURI());
     }
 }
